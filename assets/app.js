6461dfd7d32caaf74bf4000c643c8a35f6982de3
LewisSama <lou.lesage09@gmail.com>
2021-04-16 16:41:08 +0200

    Moved logo in html list, added some css to have an horizontal header

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,7 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 import './styles/normalize.css';
+import './styles/base.css';
 
 // start the Stimulus application
 import './bootstrap';
